b2b6d18cee8df62b97eaa996567d6139d6616a20
Minh Duc Nguyen <nguyenminhducdd@yahoo.de>
2020-10-17 15:53:22 +0200

    add-notion-integration

---
+++ src/pages/Calendar.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="calendar">
+    <h2>Veranstaltungen</h2>
+
+    <ul>
+      <li v-for="event in notion" :key="event.id">
+        <img v-if="event.cover" :src="event.cover" alt="cover-image" />
+
+        <div class="content">
+          <span class="date">
+            {{
+              new Date(event.date.start).toLocaleDateString("de-DE", {
+                weekday: "long",
+                year: "numeric",
+                month: "long",
+                day: "numeric"
+              })
+            }}
+          </span>
+          <div class="types">
+            <span
+              class="type"
+              v-for="ttypeName in event.type"
+              :key="ttypeName"
+              >{{ ttypeName }}</span
+            >
+          </div>
+
+          <h3>{{ event.icon }} {{ event.title }}</h3>
+          <h4>{{ event.sub_title }}</h4>
+
+          <p>
+            {{ event.description }}
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  name: "calendar",
+
+  async setup() {
+    const getNotionData = async () => {
+      const {
+        data: { result }
+      } = await axios.get("https://bfgm.herokuapp.com/bfgm-calendar")
+
+      return result
+    }
+
+    const notion = await getNotionData()
+
+    return {
+      notion: notion
+        .sort((a, b) => new Date(a.date.start) - new Date(b.date.start))
+        .filter(event => {
+          let now = new Date()
+          now.setDate(now.getDate() - 14)
+          return new Date(event.date.start) > now && !event.anfrage
+        })
+        .map(event => ({
+          ...event,
+          cover: event.cover
+            ? event.cover.indexOf("https://") !== -1
+              ? "https://www.notion.so/image/" +
+                event.cover.replace(/\//g, "%2F").replace(/:/g, "%3A") +
+                "?table=block&id=" +
+                event.id
+              : "https://www.notion.so" + event.cover
+            : null
+        }))
+        .filter(event => event.is_public)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.calendar {
+  width: 100vw;
+
+  h2 {
+    text-align: center;
+  }
+
+  ul {
+    margin: 0;
+    padding: 24px;
+    list-style: none;
+
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+
+    li {
+      overflow: hidden;
+
+      margin: 16px;
+      background-color: rgb(222, 217, 176);
+      border-radius: 4px;
+      box-shadow: 0 3px 15px 5px rgba(22, 66, 102, 0.1);
+      border: 1px solid rgba(156, 152, 122, 0.1);
+      transition: all 300ms;
+
+      &:hover {
+        box-shadow: 0 3px 15px 5px rgba(22, 66, 102, 0.15);
+        border: 1px solid rgba(156, 152, 122, 0.15);
+      }
+
+      img {
+        height: 150px;
+        width: 100%;
+
+        @media (max-width: 550px) {
+          height: 250px;
+        }
+
+        object-fit: cover;
+        object-position: center;
+      }
+
+      .content {
+        padding: 8px;
+
+        h3 {
+          line-height: 17px;
+        }
+        h4 {
+          line-height: 13px;
+          font-weight: normal;
+          font-style: italic;
+        }
+
+        .date {
+        }
+
+        .types {
+          .type {
+            font-size: 12px;
+            background-color: rgb(85, 115, 108);
+            color: white;
+            margin-right: 8px;
+            border-radius: 8px;
+            padding: 4px 8px;
+          }
+        }
+
+        p {
+          line-height: 13px;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/App.vue
+++ src/views/App.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="app">
     <Home />
+
+    <Suspense>
+      <template #default>
+        <Calendar />
+      </template>
+      <template #fallback>
+        <div class="loader">
+          <div class="lds-roller">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </template>
+    </Suspense>
+
     <Impress />
   </div>
 </template>
@@ -9,41 +30,11 @@
 // pages
 import Home from "@/pages/Home"
 import Impress from "@/pages/Impress"
+import Calendar from "@/pages/Calendar"
 
 export default {
   name: "app",
-  components: { Home, Impress }
-  // async setup() {
-  // const getNotionData = async () => {
-  //   const { data } = await axios.post(
-  //     "https://www.notion.so/api/v3/queryCollection",
-  //     {
-  //       collectionId: "526d2e7a-fdd2-4479-9b30-acd286887014",
-  //       collectionViewId: "eba83c51-94de-4982-92f2-eeaf1fe8820a",
-  //       query: { calendar_by: "Cui^" },
-  //       loader: {
-  //         type: "calendar",
-  //         dateRangeStart: 1595800800000,
-  //         dateRangeEnd: 1601848799999,
-  //         searchQuery: "",
-  //         userTimeZone: "Europe/Berlin",
-  //         startDayOfWeek: 1,
-  //         scale: "month"
-  //       }
-  //     },
-  //     {
-  //       headers: { "Access-Control-Allow-Origin": "*" }
-  //     }
-  //   )
-  //   return data
-  // }
-  //
-  // const notion = await getNotionData()
-  //
-  // return {
-  //   notion
-  // }
-  // }
+  components: { Home, Impress, Calendar }
 }
 </script>
 
@@ -51,5 +42,97 @@ export default {
 .app {
   width: 100vw;
   height: 100vh;
+
+  .loader {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 40px 0;
+  }
+
+  .lds-roller {
+    display: inline-block;
+    position: relative;
+    width: 20px;
+    height: 20px;
+  }
+  .lds-roller div {
+    animation: lds-roller 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    transform-origin: 40px 40px;
+  }
+  .lds-roller div:after {
+    content: " ";
+    display: block;
+    position: absolute;
+    width: 7px;
+    height: 7px;
+    border-radius: 50%;
+    background: rgb(13, 23, 48);
+    margin: -4px 0 0 -4px;
+  }
+  .lds-roller div:nth-child(1) {
+    animation-delay: -0.036s;
+  }
+  .lds-roller div:nth-child(1):after {
+    top: 63px;
+    left: 63px;
+  }
+  .lds-roller div:nth-child(2) {
+    animation-delay: -0.072s;
+  }
+  .lds-roller div:nth-child(2):after {
+    top: 68px;
+    left: 56px;
+  }
+  .lds-roller div:nth-child(3) {
+    animation-delay: -0.108s;
+  }
+  .lds-roller div:nth-child(3):after {
+    top: 71px;
+    left: 48px;
+  }
+  .lds-roller div:nth-child(4) {
+    animation-delay: -0.144s;
+  }
+  .lds-roller div:nth-child(4):after {
+    top: 72px;
+    left: 40px;
+  }
+  .lds-roller div:nth-child(5) {
+    animation-delay: -0.18s;
+  }
+  .lds-roller div:nth-child(5):after {
+    top: 71px;
+    left: 32px;
+  }
+  .lds-roller div:nth-child(6) {
+    animation-delay: -0.216s;
+  }
+  .lds-roller div:nth-child(6):after {
+    top: 68px;
+    left: 24px;
+  }
+  .lds-roller div:nth-child(7) {
+    animation-delay: -0.252s;
+  }
+  .lds-roller div:nth-child(7):after {
+    top: 63px;
+    left: 17px;
+  }
+  .lds-roller div:nth-child(8) {
+    animation-delay: -0.288s;
+  }
+  .lds-roller div:nth-child(8):after {
+    top: 56px;
+    left: 12px;
+  }
+  @keyframes lds-roller {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 }
 </style>

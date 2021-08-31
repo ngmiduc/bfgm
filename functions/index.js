7388b77ad37100c822f5c26be526bbaf833ebec0
Minh Duc Nguyen <nguyenminhducdd@yahoo.de>
2021-08-31 14:44:04 +0200

    new: schedule standardize function to standardize calendar notion

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,82 @@ const { Client } = require("@notionhq/client")
 
 const DB = "54a05f4c38b645a893af5636e6fce91a"
 
+const standardizeCalendar = async () => {
+  const notion = new Client({
+    auth: functions.config().notion.key
+  })
+
+  const { results: databasePages } = await notion.databases.query({
+    database_id: DB
+  })
+  console.log("QUERY databasePages")
+
+  await Promise.all(
+    databasePages
+      .filter(item => item.properties.status.select === null)
+      .map(async _page =>
+        notion.pages.update({
+          page_id: _page.id,
+          properties: {
+            status: {
+              select: { name: "intern" }
+            }
+          }
+        })
+      )
+  )
+  console.log("UPDATE no status")
+
+  await Promise.all(
+    databasePages
+      .filter(item => item.properties.Veranstalter.select === null)
+      .map(async _page =>
+        notion.pages.update({
+          page_id: _page.id,
+          properties: {
+            Veranstalter: { select: { name: "<kein>" } }
+          }
+        })
+      )
+  )
+  console.log("UPDATE no VA")
+
+  await Promise.all(
+    databasePages
+      .filter(item => item.properties.Format.multi_select.length === 0)
+      .map(async _page =>
+        notion.pages.update({
+          page_id: _page.id,
+          properties: {
+            Format: { multi_select: [{ name: "<kein>" }] }
+          }
+        })
+      )
+  )
+  console.log("UPDATE no format")
+}
+
+exports.scheduleStandardizeCalendar = functions
+  .region("europe-west3")
+  .pubsub.schedule("0 0 1 * *")
+  .timeZone("Europe/London")
+  .onRun(async () => {
+    console.log("schedule standardize calendar")
+
+    await standardizeCalendar()
+  })
+
+exports.runStandardizeCalendar = functions
+  .region("europe-west3")
+  .https.onCall(async () => {
+    console.log("schedule standardize calendar")
+
+    await standardizeCalendar()
+  })
+
 exports.getEvents = functions.region("europe-west3").https.onCall(async () => {
+  console.log("Get calendar events!")
+
   const notion = new Client({
     auth: functions.config().notion.key
   })
